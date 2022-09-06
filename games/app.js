f03ac13972e7b9d3ae21fe2a903ccd5ff5eb0b0a
Bunnie <bunniecodes@gmail.com>
2022-09-05 22:09:27 -0700

    Got madlibs working using string literals and ids to fill in the blanks.  Uses the information/values entered in the form and inputs it to the text

--- games/app.js
+++ games/app.js
@@ -1,10 +1,12 @@
-const btn = document.getElementById('mad-libs-button')
-const body = document.getElementById('mad-libs-text')
+const btn = document.getElementById("mad-libs-button");
+// let form = new FormData();
+const body = document.getElementById("mad-libs-text");
 
 btn.addEventListener('click', () => {
     showMadLib();
 });
 
 function showMadLib() {
-    body.innerHTML = "Today I went to the zoo. I saw a(n) {adjList[0]} {nounList[0]} jumping up and down in its tree. He {verbList[0]} {advList[0]} through the large tunnel that led to its {adjList[1]} {nounList[1]}. I got some peanuts and passed them through the cage to a gigantic gray {nounList[2]} towering above my head. Feeding that animal made me hungry. I went to get a {adjList[2]} scoop of ice cream. It filled my stomach. Afterwards I had to {verbList[1]} {advList[1]} to catch our bus. When I got home I {verbList[2]} my mom for a {adjList[3]} day at the zoo.";
-}
+    // body.innerHTML = (`Today I went to the zoo. I saw a(n) ${adj1.value} ${noun1.value} jumping up and down in its tree. He ${verb1.value} ${adv1.value} through the large tunnel that led to its ${adj2.value} ${noun2.value}.`);
+    body.innerHTML = (`Today I went to the zoo. I saw a(n) ${adj1.value} ${noun1.value} jumping up and down in its tree. He ${verb1.value} ${adv1.value} through the large tunnel that led to its ${adj2.value} ${noun2.value}. I got some peanuts and passed them through the cage to a gigantic gray ${noun3.value} towering above my head. Feeding that animal made me hungry. I went to get a ${adj3.value} scoop of ice cream. It filled my stomach. Afterwards I had to ${verb2.value} ${adv2.value} to catch our bus. When I got home I ${verb3.value} my mom for a ${adj4.value} day at the zoo.`);
+}   
