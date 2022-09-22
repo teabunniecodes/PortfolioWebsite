788d152b094888a69ccb6f87219cec6910956857
Bunnie <bunniecodes@gmail.com>
2022-09-22 16:02:36 -0700

    Event Listeners for buttons and Key Up are now working and active.  To do next time is to tie the key up to activate the button and pull the chosen word from the server side

--- website/static/javascript/hangman.js
+++ website/static/javascript/hangman.js
@@ -1,1 +1,26 @@
-const words = loadStrings(textdocuments/hangman-wordlist.txt)
+// const words = loadStrings(textdocuments/hangman-wordlist.txt)
+const keyboardButton = document.getElementsByClassName("keyboard-button")
+const keyboardAction = document.getElementsByClassName("keyboard-action")
+const guess = document.getElementById("letter-guess")
+document.onkeyup = checkKey
+
+for (let x=0; x < keyboardButton.length; x++) {
+    keyboardButton[x].addEventListener("click", e => {
+        console.log(keyboardButton[x].value);
+    });
+    if (e.key === keyboardButton[x]) {
+        console.log(e.key + " matches the button");
+        }
+}
+
+
+function checkKey(e) {
+    // if (letters.includes(e.key)) {
+        if (e.key.charCodeAt(0) >= 97 && e.key.charCodeAt(0) <=122) {
+            console.log(e.key + " is a letter");
+        }
+        else {
+            console.log(e.key + " Not a letter")
+        }
+    };
+        // }
