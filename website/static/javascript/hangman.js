eaa9cc5422c0a6f3edf3bae9e3d27841d6ef36af
Bunnie <bunniecodes@gmail.com>
2022-09-27 16:31:56 -0700

    Created function to check if the key press down matches a button and toggles the class of the button so it becomes highlighted

--- website/static/javascript/hangman.js
+++ website/static/javascript/hangman.js
@@ -1,6 +1,3 @@
-
-//TODO WRITE OUT MY PSUEDO CODE D: RAWRRRRRRRRRR
-
 // const words = loadStrings(textdocuments/hangman-wordlist.txt)
 const keyboardButton = document.getElementsByClassName("keyboard-button")
 const guessLetter = document.getElementById("letter-guess")
@@ -9,17 +6,31 @@ const turnsLeft = document.getElementById("turns-left")
 const guessList = []
 document.onkeyup = checkKey
 // document.onkeyup = submitGuess
+document.onkeydown = checkButton
 
-let turns = 6
+let turns = 7
 
-function checkButton() {
-    for (let x=0; x < keyboardButton.length; x++) {
-        keyboardButton[x].addEventListener("click", e => {
-            console.log(keyboardButton[x].value);
-        });
-        // if (e.key === keyboardButton[x]) {
-        //     console.log(e.key + " matches the button");
-        //     }
+// What I want to accomplish in hangman GUI
+// buttons to flash if cooresponding key is pressed (can I use key value to = button value?)
+    // figure out if I can have CSS on JS side (use inner HTML tag to add the style?)
+    // when button is pressed, activates a guess as well
+    // changes colors for the buttons if it is a correct guess or wrong guess (green/red)
+// need to check the guess with the list of letter guesses already for duplicate letters
+// need to check the letters with the word on server side to see if it is in the word
+// create a way to have the user guess an entire word - have Enter and Backspace active during this time
+// checks guessed word against server side word - if wrong Game over automatically
+
+for (let x=0; x < keyboardButton.length; x++) {
+    keyboardButton[x].addEventListener("click", e => {
+        console.log("Button Pressed");
+    });
+}
+
+function checkButton(e) {
+    let button = document.getElementById(e.key)
+    const classes = button.classList
+    if ((e.key == button.value) && (button.classList != "colored-button")) {
+        classes.toggle("colored-button", "keyboard-button");
     }
 }
 
@@ -27,14 +38,14 @@ function checkKey(e) {
     if (turns > 0) {
         if ((e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 97 && e.keyCode <= 122)) {
             console.log(e.key + " is a letter");
-            // let letter = e.key
+            let letter = e.key
             validGuess(e.key);
-            addGuess(e.key)
+            addGuess(e.key);
         }
     }
-    if (turns === 0) {
-        gameOver(e.key)
-    }
+    // if (turns === 0) {
+    //     gameOver(e.key)
+    // }
 }
 // function submitGuess(e)
 //     if (e.keyCode == 13) {
